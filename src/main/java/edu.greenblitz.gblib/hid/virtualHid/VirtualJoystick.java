bad72eaf98ff242531c06908304a39e1074f182a
unknown <greenblitz.4590@gmail.com>
2022-01-19 20:48:04 +0200

    asaf - removed unnecessery paramter in constructor and unused classes

--- src/main/java/edu.greenblitz.gblib/hid/virtualHid/VirtualJoystick.java
+++ src/main/java/edu.greenblitz.gblib/hid/virtualHid/VirtualJoystick.java
@@ -42,39 +42,10 @@ public class VirtualJoystick extends Joystick {
         zeroHashMap.put(20, 0.0);
     }
 
-    private JoystickButton A;
-    private JoystickButton B;
-    private JoystickButton X;
-    private JoystickButton Y;
-    private JoystickButton L1;
-    private JoystickButton R1;
-    private JoystickButton START;
-    private JoystickButton BACK;
-    private JoystickButton L3;
-    private JoystickButton R3;
-    private POVButton POV_UP;
-    private POVButton POV_RIGHT;
-    private POVButton POV_DOWN;
-    private POVButton POV_LEFT;
 
-    public VirtualJoystick(GenericHID ourHID, HashMap<Double, HashMap<Integer, Double>> myFollowDriverData) {
+    public VirtualJoystick( HashMap<Double, HashMap<Integer, Double>> myFollowDriverData) {
         super(-1);//there is no port and no port is needed.
-        this.ourHID = ourHID;
         this.myFollowDriverData = myFollowDriverData;
-        A = new JoystickButton(ourHID, 1);
-        B = new JoystickButton(ourHID, 2);
-        X = new JoystickButton(ourHID, 3);
-        Y = new JoystickButton(ourHID, 4);
-        L1 = new JoystickButton(ourHID, 5);
-        R1 = new JoystickButton(ourHID, 6);
-        START = new JoystickButton(ourHID, 7);
-        BACK = new JoystickButton(ourHID, 8);
-        L3 = new JoystickButton(ourHID, 9);
-        R3 = new JoystickButton(ourHID, 10);
-        POV_UP = new POVButton(ourHID, 0);
-        POV_RIGHT = new POVButton(ourHID, 90);
-        POV_DOWN = new POVButton(ourHID, 180);
-        POV_LEFT = new POVButton(ourHID, 270);
         startTime = System.currentTimeMillis() / 1000.0;
     }
 
