5ddf66bf093fa571e7e22ff72cfe1ddd8fd42144
itamaroryan <itamar.oryan@gmail.com>
2022-06-08 12:18:08 +0300

    tal - finished rewriting Chassis, should work rn

--- src/main/java/edu.greenblitz.gblib/subsystems/Chassis.java
+++ src/main/java/edu.greenblitz.gblib/subsystems/Chassis.java
@@ -1,25 +1,15 @@
 package edu.greenblitz.gblib.subsystems;
 
 import com.ctre.phoenix.sensors.PigeonIMU;
-import com.revrobotics.CANSparkMax;
 import edu.greenblitz.gblib.gyro.PigeonGyro;
-import edu.greenblitz.gblib.hid.SmartJoystick;
 import edu.greenblitz.gblib.motors.AbstractMotor;
-import edu.greenblitz.gblib.motors.GBFalcon;
 import edu.greenblitz.gblib.motors.Motor;
 import edu.greenblitz.gblib.motors.MotorType;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
-
-import javax.swing.text.Position;
-
-import static edu.wpi.first.wpilibj.smartdashboard.SmartDashboard.putString;
-
 
 public class Chassis extends GBSubsystem {
 	private static Chassis instance;
 	private final PigeonGyro gyroscope;
-	private double wheelDistance;
+	private final double wheelDistance;
 	private final Motor[] motors;
 	
 	
@@ -38,72 +28,64 @@ public class Chassis extends GBSubsystem {
 		this.wheelDistance = wheelDistance;
 	}
 	
-		public static Chassis getInstance() {
+	public static Chassis getInstance() {
 		return instance;
 	}
 	
-	public void setIdleMode(AbstractMotor.IdleMode idleMode){
-		for (Motor motor: motors) {
+	public void setIdleMode(AbstractMotor.IdleMode idleMode) {
+		for (Motor motor : motors) {
 			motor.setIdleMode(idleMode);
 		}
 	}
-
+	
 	public static void create(
 			MotorType motorType,
 			int[] ports,
 			boolean[] isInverted,
-			double wheelDistance )
-	{instance= new Chassis(motorType,ports,isInverted,wheelDistance);}
+			double wheelDistance) {
+		instance = new Chassis(motorType, ports, isInverted, wheelDistance);
+	}
 	
-	public Motor[] getRightMotors(){
-		Motor[] right= new Motor[motors.length/2];
-		for (int i = 0; i <motors.length/2; i++) {
-			right[i] =motors[i];
+	public Motor[] getRightMotors() {
+		Motor[] right = new Motor[motors.length / 2];
+		for (int i = 0; i < motors.length / 2; i++) {
+			right[i] = motors[i];
 		}
 		return right;
 	}
 	
-	public Motor[] getLeftMotors(){
-		Motor[] left= new Motor[motors.length/2];
-		for (int i = 0; i <motors.length/2; i++) {
-			left[i] =motors[motors.length/2+i];
+	public Motor getRightMotors(int id) {
+		Motor[] rightMotors = getRightMotors();
+		return rightMotors[id];
+	}
+	
+	public Motor[] getLeftMotors() {
+		Motor[] left = new Motor[motors.length / 2];
+		for (int i = 0; i < motors.length / 2; i++) {
+			left[i] = motors[motors.length / 2 + i];
 		}
 		return left;
 	}
 	
-//
-//	public void moveMotors(double left, double right) {
-//		SmartDashboard.putNumber("Left Power", left);
-//		SmartDashboard.putNumber("Right Power", right);
-//		for (int i = 0; i < motors.length/2; i++) {
-//			motors[i].setPower(right);
-//		}
-//		for (int i = motors.length/2 +1 ; i <motors.length ; i++) {
-//			motors[i].setPower(left);
-//		}
-////
-////		motors[0].setPower(right);
-////		motors[1].setPower(right);
-////		motors[2].setPower(right);
-////		motors[3].setPower(left);
-//	}
-//
-//	public void moveMotors(double power){
-//		moveMotors(power, power);
-//	}
-//
-//	public void toBrake() {
-//		for(Motor motor : motors){
-//			motor.setIdleMode(AbstractMotor.IdleMode.Brake);
-//		}
-//	}
-//
-//	public void toCoast() {
-//		for(Motor motor : motors){
-//			motor.setIdleMode(AbstractMotor.IdleMode.Coast);
-//		}
-//	}
-//
+	public Motor getLeftMotors(int id) {
+		Motor[] leftMotors = getLeftMotors();
+		return leftMotors[id];
+	}
+	
+	public void moveMotors(double powerLeft, double powerRight) {
+		for (Motor motor : getLeftMotors()) {
+			motor.setPower(powerLeft);
+		}
+		for (Motor motor : getRightMotors()) {
+			motor.setPower(powerRight);
+		}
+	}
+	
+	public void moveMotors(double power) {
+		moveMotors(power, power);
+	}
+
+
 //	public void semiToCoast() {
 //		for(int i = 1; i < 5; i++){
 //			motors[i].setIdleMode(AbstractMotor.IdleMode.Coast); //moves two thirds to coast
@@ -115,85 +97,84 @@ public class Chassis extends GBSubsystem {
 //			motors[i].setIdleMode(AbstractMotor.IdleMode.Brake); //moves two thirds to brake
 //		}
 //	}
-//
-//	public void arcadeDrive(double moveValue, double rotateValue) {
+	
+	public void arcadeDrive(double moveValue, double rotateValue) {
+		moveMotors(moveValue - rotateValue, moveValue + rotateValue);
+	}
+	
+	//	public void arcadeDrive(double moveValue, double rotateValue) {
 //		SmartDashboard.putNumber("rotate power", rotateValue);
 //		moveMotors(moveValue - rotateValue, moveValue + rotateValue);
 //	}
 //
-//	public double getLeftMeters() {
-//		return motors[1].getNormalizedPosition();
-//	}
 //
-//	public double getRightMeters() {
-//		return motors[motors.length/2+1].getNormalizedPosition();
-//	}
-//
-//	public double getMeters(){
-//		return (getLeftMeters() + getRightMeters()) / 2.0;
-//	}
-//
-//	public double getLeftRate() {
-//		return motors[1].getNormalizedVelocity();
-//	}
-//
-//	public double getRightRate() {
-//		return motors[motors.length/2+1].getNormalizedVelocity();
-//	}
-//
-//	public double getLinearVelocity() {
-//		return 0.5 * (getRightRate() + getLeftRate());
-//	}
-//
-//	public double getAngularVelocityByWheels() {
+	public double getLeftMeters() {
+		return getLeftMotors(1).getNormalizedPosition();
+	}
+	
+	public double getRightMeters() {
+		return getRightMotors(1).getNormalizedPosition();
+	}
+	
+	public double getMeters() {
+		return (getLeftMeters() + getRightMeters()) * 0.5;
+	}
+	
+	public double getLeftVelocity() {
+		return getLeftMotors(1).getNormalizedVelocity();
+	}
+	
+	public double getRightVelocity() {
+		return getRightMotors(1).getNormalizedVelocity();
+	}
+	
+	
+	public double getLinearVelocity() {
+		return (getRightVelocity() + getLeftVelocity()) * 0.5;
+	}
+	
+	//	public double getAngularVelocityByWheels() {
 //		return (getRightRate() - getLeftRate())/getWheelDistance();
 //		//this is true, if u don't understand, go to your mommy and cry (ask asaf);
 //	}
 //
-//	public double getAngle() {
-//		return gyroscope.getNormalizedYaw();
-//	}
-//
-//	public double getRawAngle() {
-//		return gyroscope.getRawYaw();
-//	}
-//
-//	public double getAngularVelocityByGyro() {
-//		return gyroscope.getYawRate();
-//	}
-//
-//	public void resetGyro() {
-//		gyroscope.reset();
-//	}
-//
-//	public double getWheelDistance() {
-//		return wheelDistance;
-//	}
+	public double getAngle() {
+		return gyroscope.getNormalizedYaw();
+	}
+	
+	public double getRawAngle() {
+		return gyroscope.getRawYaw();
+	}
+	
+	public double getAngularVelocityByGyro() {
+		return gyroscope.getYawRate();
+	}
+	
+	public void resetGyro() {
+		gyroscope.reset();
+	}
+	
+	public double getWheelDistance() {
+		return wheelDistance;
+	}
 //
 //	public Position getLocation() { //todo todo todo fzxtdjyygvju
 //		return Localizer.getInstance().getLocation();
 //	}
-//
-//
-//
-//
 //	@Override
 //	public void periodic() {
 //		SmartDashboard.putNumber("Pigeon angle deg", Math.toDegrees(getAngle()));
 //		putString("Location", Chassis.getInstance().getLocation().toString());
-//
 //	}
-//
-//	public void resetEncoders() {
-//		.reset();
-//		leftEncoder.reset();
-//	}
-//
-//	public void setMotorByID(int id, double power){
-//		motors[id].set(power);
-//	}
-//
-//
-//
-//
+	
+	public void resetEncoders() {
+		getLeftMotors(1).resetEncoder();
+		getRightMotors(1).resetEncoder();
+	}
+	
+	public void setMotorByID(int id, double power) {
+		motors[id].setPower(power);
+	}
+	
+	
 }
