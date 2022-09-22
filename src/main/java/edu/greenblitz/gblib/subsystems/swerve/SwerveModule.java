92fcfdb6abcfb8d2322fbb3cf99a57f487e782dc
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2022-09-22 14:55:05 +0300

    Amir & Asaf - added adhd to swerve module

--- src/main/java/edu/greenblitz/gblib/subsystems/swerve/SwerveModule.java
+++ src/main/java/edu/greenblitz/gblib/subsystems/swerve/SwerveModule.java
@@ -12,66 +12,66 @@ import edu.wpi.first.wpilibj.AnalogInput;
 public class SwerveModule {
 
 
-    private int isReversed = 1;
-    public double targetAngle;
-    public double targetVel;
-	private  double maxLampreyVal;
+	private int isReversed = 1;
+	public double targetAngle;
+	public double targetVel;
+	private double maxLampreyVal;
 	private double minLampreyVal;
-    private final GBMotor angleMotor;
-    private final GBMotor linearMotor;
-    private final AnalogInput lamprey;
+	private final GBMotor angleMotor;
+	private final GBMotor linearMotor;
+	private final AnalogInput lamprey;
 	private final SimpleMotorFeedforward feedforward;
 
-    public SwerveModule(IMotorFactory motorFactoryA, IMotorFactory motorFactoryL, int portA, int portL,
-                        int lampreyID, double maxLampreyVal, double minLampreyVal,
-                        PIDObject pidAng , PIDObject pidLin, SimpleMotorFeedforward feedforward) {
-        angleMotor = motorFactoryA.generate(portA);
-        linearMotor = motorFactoryL.generate(portL);
-        lamprey = new AnalogInput(lampreyID);
-        lamprey.setAverageBits(2);
+	public SwerveModule(IMotorFactory motorFactoryA, IMotorFactory motorFactoryL, int portA, int portL,
+	                    int lampreyID, double maxLampreyVal, double minLampreyVal,
+	                    PIDObject pidAng, PIDObject pidLin, SimpleMotorFeedforward feedforward) {
+		angleMotor = motorFactoryA.generate(portA);
+		linearMotor = motorFactoryL.generate(portL);
+		lamprey = new AnalogInput(lampreyID);
+		lamprey.setAverageBits(2);
 		this.maxLampreyVal = maxLampreyVal;
 		this.minLampreyVal = minLampreyVal;
 		configAnglePID(pidAng);
 		configLinPID(pidLin);
 		this.feedforward = feedforward;
-    }
+	}
 
-    public double getLampreyAngle() { // in radians;
-        return (lamprey.getValue()- minLampreyVal)/(maxLampreyVal-minLampreyVal) *Math.PI*2;
-    }
+	public double getLampreyAngle() { // in radians;
+		return (lamprey.getValue() - minLampreyVal) / (maxLampreyVal - minLampreyVal) * Math.PI * 2;
+	}
 
-    public void rotateToAngle(double angle) {
-        DualSidedAngTarget dualSidedAngTarget = DualSidedAngTarget.generateTarget(angle, GBMath.modulo(getMotorAngle(), 2*Math.PI));
-        angle = dualSidedAngTarget.getTarget() ;
-		
-		angle += getMotorAngle() - GBMath.modulo(getMotorAngle(), 2*Math.PI);
-		
-        isReversed = dualSidedAngTarget.getDirection();
-        angleMotor.setTargetByPID(angle, AbstractMotor.PIDTarget.Position);
-        targetAngle = angle;
-    }
+	public void rotateToAngle(double angle) {
+		DualSidedAngTarget dualSidedAngTarget = DualSidedAngTarget.generateTarget(angle, GBMath.modulo(getMotorAngle(), 2 * Math.PI));
+		angle = dualSidedAngTarget.getTarget();
 
-	public double getRawLampreyAngle(){
+		angle += getMotorAngle() - GBMath.modulo(getMotorAngle(), 2 * Math.PI);
+
+		isReversed = dualSidedAngTarget.getDirection();
+		angleMotor.setTargetByPID(angle, AbstractMotor.PIDTarget.Position);
+		targetAngle = angle;
+	}
+
+	public double getRawLampreyAngle() {
 		return lamprey.getValue();
 	}
-	
+
 	public double getMotorAngle() {
 		return angleMotor.getNormalizedPosition();
 	}
-	
+
 	public double getCurrentVel() {
 		return linearMotor.getNormalizedVelocity();
 	}
 
-    public void rotateByAngle(double angle) {
+	public void rotateByAngle(double angle) {
 		angleMotor.setTargetByPID(getMotorAngle() + angle, AbstractMotor.PIDTarget.Position);
-    }
+	}
 
-    public void resetAngle() {
-        angleMotor.resetEncoder();
-    }
+	public void resetAngle() {
+		angleMotor.resetEncoder();
+	}
 
-	public void resetEncoderByLamprey(){
+	public void resetEncoderByLamprey() {
 		angleMotor.setEncoderAng(getLampreyAngle());
 	}
 
@@ -79,35 +79,35 @@ public class SwerveModule {
 		linearMotor.configurePID(pidObject);
 	}
 
-    public void configAnglePID(PIDObject pidObject) {
-        angleMotor.configurePID(pidObject);
-    }
-
-	
-	public void setLinSpeed(double speed){
-		linearMotor.setVoltage(feedforward.calculate(speed));
+	public void configAnglePID(PIDObject pidObject) {
+		angleMotor.configurePID(pidObject);
 	}
-	
-	public void setRotPower(double power){
+
+
+	public void setLinSpeed(double speed) {
+		linearMotor.setVoltage(feedforward.calculate(speed * isReversed));
+	}
+
+	public void setRotPower(double power) {
 		angleMotor.setPower(power);
 	}
 	//only for debugging
 
-    public double getTargetAngle() {
-        return targetAngle;
-    }
-
-    public double getTargetVel() {
-        return targetVel* isReversed;
-    }
-
-	public void setLinPower(double power){
-		linearMotor.setPower(power);
+	public double getTargetAngle() {
+		return targetAngle;
 	}
 
-    public int getIsReversed() {
-        return isReversed;
-    }
+	public double getTargetVel() {
+		return targetVel * isReversed;
+	}
+
+	public void setLinPower(double power) {
+		linearMotor.setPower(power * isReversed);
+	}
+
+	public int getIsReversed() {
+		return isReversed;
+	}
 
 
 }
